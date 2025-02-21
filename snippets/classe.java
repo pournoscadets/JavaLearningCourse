15973aa9364bcba8e045be1769ca456d4242aeda
akoua <akouayao225@gmail.com>
2025-02-21 20:43:24 +0100

    first commit

---
+++ snippets/classe.java
@@ -0,0 +1,9 @@
+// #region snippet
+
+public class Classe {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// #endregion snippet
---
+++ snippets/external.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+// #region snippet
+// Inside ./snippets/external.ts
+export function emptyArray<T>(length: number) {
+  return Array.from<T>({ length })
+}
+
+export function sayHello() {
+  console.log('Hello from snippets/external.ts')
+}
+
+// #endregion snippet
+
+
